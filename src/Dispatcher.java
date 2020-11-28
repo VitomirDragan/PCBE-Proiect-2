de276e5b35889522d4f1fdda30e7771bb86306ec
VitomirDragan <vitomir_dragan86@yahoo.com>
2020-11-28 11:36:09 +0200

    Dispatcher

--- src/Dispatcher.java
+++ src/Dispatcher.java
@@ -1,25 +1,57 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+
 public class Dispatcher implements Runnable {
 
-    //add parameters + constructor
+    private Queue<Event> eventQueue;
+    private HashMap<String, ArrayList<Actor>> subscribedReaders = new HashMap<String, ArrayList<Actor>>();
+    private HashMap<String, ArrayList<Subscription>> eventSubscriptions = new HashMap<String, ArrayList<Subscription>>();
 
     public void run()
     {
+        while(true){
+            processNextEvent();
+        }
+    }
 
+    public void processNextEvent(){
+        try {
+            Event event = eventQueue.remove();
+        }
+        catch(NoSuchElementException exec){
+            System.out.println(exec);
+        }
+
+        Event e = eventQueue.remove();
+        ArrayList<Subscription> subscriptions = eventSubscriptions.get(e.getEventType());
+        for(Subscription subscription : subscriptions){
+            subscription.getActor().myNotify(e);
+        }
     }
 
     public void postEvent(Event e)
     {
-        //TODO
+        eventQueue.add(e);
     }
 
     public void acceptEventSubscription(String domainType, Actor actor)
     {
-        //TODO
+        for(Event e : eventQueue) {
+            if (e.getEventType().equals(domainType))
+            {
+                ArrayList<Actor> readers = subscribedReaders.get(domainType);
+                readers.add(actor);
+                subscribedReaders.put(domainType, readers);
+            }
+        }
     }
 
     public void acceptDomainSubscription(String domain, Actor actor, Filter filter)
     {
-        //TODO
+        Subscription subscription = new Subscription(actor, domain, filter);
+        ArrayList<Subscription> subscriptions = eventSubscriptions.get()
     }
 
 }
